07940a0db9b09a96e84d5b2924bebda2fc2971be
Susan Johnston <sjohnston84@gmail.com>
2019-07-04 13:05:26 +0100

    fix crimap.path on Linux

--- R/run_crimap_prepare.R
+++ R/run_crimap_prepare.R
@@ -113,7 +113,10 @@ run_crimap_prepare <- function(genfile, build = FALSE, snplist = NULL, snpinsert
   } else {
     
     if(Sys.info()["sysname"] == "Linux"){
-      crimap.path <- paste0(.libPaths()[length(.libPaths())], "/crimaptools/bin/linux/crimap")
+      crimap.path <- paste0(.libPaths()[1], "/crimaptools/bin/linux/crimap")
+      if(!file.exists(crimap.path)){
+         crimap.path <- paste0(.libPaths()[length(.libPaths())], "/crimaptools/bin/linux/crimap")
+      }
     } else {
       crimap.path <- paste0(.libPaths()[length(.libPaths())], "/crimaptools/bin/macos/crimap")
     }
