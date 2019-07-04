43679eeb7ac00f20fa7c9cbcf99ee9c2c1fd26dd
Susan Johnston <Susan.Johnston@ed.ac.uk>
2019-07-04 15:10:22 +0100

    updated parse_map to output the sex-averaged map as well as sex specific maps

--- R/parse_map.R
+++ R/parse_map.R
@@ -6,8 +6,24 @@
 parse_map <- function(mapfile){
 
   x <- readLines(mapfile)
-  map <- x[grep("Sex-specific map", x):length(x)]
 
+  map1 <- x[grep("Sex_averaged map", x):(grep("Sex-specific map", x)-1)]
+
+  map1 <- map1[3:length(map1)]
+  map1 <- map1[-which(map1 == "")]
+  map1 <- map1[1:(length(map1)-2)]
+
+  map1 <- data.frame(map1, stringsAsFactors=F)
+
+  map1$map2 <- NA
+  for(i in seq(1, nrow(map1), 2)) map1$map2[i] <- map1$map1[i+1]
+  map1 <- map1[seq(1, nrow(map1), 2),]
+
+  for(i in 1:1e4) if(length(grep("  ", map1$map1) > 0)) map1$map1  <- gsub("  ", " ", map1$map1) else break
+  for(i in 1:1e4) if(length(grep("  ", map1$map2) > 0)) map1$map2  <- gsub("  ", " ", map1$map2) else break
+
+
+  map <- x[grep("Sex-specific map", x):length(x)]
 
   map <- map[3:length(map)]
   map <- map[1:(length(map)-7)]
@@ -22,8 +38,6 @@ parse_map <- function(mapfile){
   for(i in 1:1e4) if(length(grep("  ", map$map)  > 0)) map$map   <- gsub("  ", " ", map$map)  else break
   for(i in 1:1e4) if(length(grep("  ", map$map2) > 0)) map$map2  <- gsub("  ", " ", map$map2) else break
 
-  rm(x)
-
   map$Order <- NA
   map$SNP.Name <- NA
   map$cMPosition.Female <- NA
@@ -32,29 +46,42 @@ parse_map <- function(mapfile){
   map$cMdiff.Female <- NA
   map$Male.r <- NA
   map$cMdiff.Male <- NA
+  map$cMPosition <- NA
+  map$r <- NA
+  map$cMdiff <- NA
+
+  rm(x)
 
   for(i in 1:nrow(map)){
 
     x <- strsplit(map$map[i], split = " ")[[1]]
     if(length(-which(x == "")) > 0) x <- x[-which(x == "")]
 
+    x1 <- strsplit(map1$map1[i], split = " ")[[1]]
+    if(length(-which(x1 == "")) > 0) x1 <- x1[-which(x1 == "")]
+
     map$Order[i]             <- x[1]
     map$SNP.Name[i]          <- x[2]
     map$cMPosition.Female[i] <- x[3]
     map$cMPosition.Male[i]   <- x[4]
+    map$cMPosition[i]        <- x1[3]
 
-    rm(x)
+    rm(x, x1)
 
     x <- strsplit(map$map2[i], split = " ")[[1]]
     if(length(-which(x == "")) > 0) x <- x[-which(x == "")]
+    x1 <- strsplit(map1$map2[i], split = " ")[[1]]
+    if(length(-which(x1 == "")) > 0) x1 <- x1[-which(x1 == "")]
 
     map$Female.r[i]      <- x[1]
     map$cMdiff.Female[i] <- x[2]
     map$Male.r[i]        <- x[3]
     map$cMdiff.Male[i]   <- x[4]
+    map$r[i]             <- x1[1]
+    map$cMdiff[i]        <- x1[2]
 
 
-    rm(x)
+    rm(x, x1)
   }
 
   map <- subset(map, select = -c(map, map2))
@@ -70,7 +97,8 @@ parse_map <- function(mapfile){
 
   convert.cols <- which(names(map) %in%
                           c("Order", "cMPosition.Female", "cMPosition.Male",
-                            "Female.r", "cMdiff.Female", "Male.r", "cMdiff.Male"))
+                            "Female.r", "cMdiff.Female", "Male.r", "cMdiff.Male",
+                            "cMPosition", "r", "cMdiff"))
 
   map[,convert.cols] <- sapply(map[,convert.cols], as.numeric)
 
